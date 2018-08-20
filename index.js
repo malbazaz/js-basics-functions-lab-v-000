46cb95ac19584b9f6955f1ba5af64b7ca2fb2eca
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 21:32:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function calculatesFarePrice(num1, num2){
   if (dist<=400){
     price = 0;
   }else if (dist>400 && dist<=2000){
-
+    price = (dist - 400)*0.02
   }else if (dist>2000 && dist<=2500){
 
   }else {
