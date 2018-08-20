27ad81cd04de5e1fb170b784a7804ab65e9d7dac
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-20 21:23:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num){
+  let dist;
+  if(num > 42) {
+    dist = num - 42;
+  } else {
+    dist = 42 - num
+  }
+}
+
+function distanceFromHqInFeet(){
+  
+}
+
+function distanceTravelledInFeet(){
+  
+}
+
